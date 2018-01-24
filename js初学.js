bab21659201c4ec1dbffaee2d6fbb3678f8d2268
WangMuDu <1245240736@qq.com>
2018-01-24 16:13:34 +0800

    Create js初学.js

---
+++ js初学.js
@@ -0,0 +1,55 @@
+//   var n = parseInt(window.prompt('输入N'));
+//          var mul = 2;
+//          for(var i = 0; i < n; i ++){
+//              mul *= 2;
+//          }
+//          document.write(mul);
+// 输入数字的2次方
+//             var n = 2;
+//             switch(n) {
+//                 case 1:
+//                     document.write("1");
+//                     break;
+//                 case 2:
+//                     document.write("2");
+//                     break;
+//                 case 3:
+//                     document.write("3");
+//                     break;
+//             }
+//
+//         var acfun = [1,2,3,4,5,6,7,8];
+//         for(var i = 0;i < acfun.length; i ++)
+//         console.log((acfun[i]));
+// 对象
+//             var acfun ={
+//                 name:"wang",
+//                 cname:"hang",
+//                 age:40,
+//
+//             }
+//             console.log(acfun.age); //取对象值
+//
+//             function docs(jiao) {
+//                 switch (jiao){
+//
+//                     case "one":
+//                         document.write("one");
+//                         return;
+//                     case "two":
+//                         document.write("two");
+//                         return;
+//                     case "three":
+//                         document.write("three");
+//                         return;
+//                 }
+//             } //返回输出docs("one")
+//     输入数值倒着写
+//         function reverse() {
+//             var num = window.prompt('input');
+//             var str = "";
+//             for (var i = num.length - 1; i >= 0 ; i--){  //num.length长度
+//                 str += num.charAt(i)
+//             }
+//             document.write(str)
+//         }
